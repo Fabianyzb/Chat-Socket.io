5bc1c57a1f19ea864a5ba0e184546011328dcd7e
Fabian Yanez <fabian.yzb@gmail.com>
2024-04-22 02:15:16 -0400

    socket.io

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import http from "http";
+import { Server as SocketServer } from "socket.io";
 
 const app = express();
+/* socket.io */
+const server = http.createServer(app);
+const io = new SocketServer(server);
 
-app.listen(3000);
+server.listen(3000);
 console.log("Server running on port", 3000);
