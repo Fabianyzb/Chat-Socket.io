48fc7c9623f9e2748ce33c77879fba9a4a01d4a5
Fabian Yanez <fabian.yzb@gmail.com>
2024-04-24 00:39:48 -0400

    conectado clien w server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
 import io from 'socket.io-client'
+import { useState } from 'react'
+import { symbolName } from 'typescript'
 
 const socket = io("/")
 
 function App() {
+  const [message, setMessage] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //evita que el form refresque la pagina
+    console.log(message);
+  }
+
   return (
-    <div>hola</div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Write your message...'
+          onChange={(e) => setMessage(e.target.value)} /* <- actualiza valor del input cuando se cambia el mensaje */
+        />
+        <button>Send</button>
+      </form>
+    </>
   )
 }
 
